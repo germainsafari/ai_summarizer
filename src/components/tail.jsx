1ebd6e2e8ea4257cd7a9c0a956c9bb6bc16ae8ac
germainsafari <GG@ee10725997111>
2023-09-17 22:14:43 +0200

    touch

--- src/components/tail.jsx
+++ src/components/tail.jsx
@@ -4,6 +4,9 @@ function tail() {
   return (
     <>
         <div>tail</div>
+        <p>i will remember that i did this at night
+            i am addicted to the code. i should learn everything
+        </p>
     </>
 
   )
