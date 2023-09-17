7892d88f55900343b1471f05fbaf177b2d4dc594
germainsafari <GG@ee10725997111>
2023-09-17 22:17:35 +0200

    touch

--- src/components/tail.jsx
+++ src/components/tail.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function tail() {
-  return (
-    <>
-        <div>tail</div>
-        <p>i will remember that i did this at night
-            i am addicted to the code. i should learn everything
-        </p>
-    </>
+function Tail() {
+    const [quote, setQuote] = useState("");
 
-  )
+    const quotes = [
+        "First, solve the problem. Then, write the code.",
+        "Code is like humor. When you have to explain it, it’s bad.",
+        "Don't rush the process, savor the journey of learning!",
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "Talk is cheap. Show me the code.",
+    ];
+
+    const displayRandomQuote = () => {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[randomIndex]);
+    }
+
+    return (
+        <>
+            <div>tail</div>
+            <p>
+                I will remember that I did this at night.
+                I am addicted to the code. I should learn everything.
+            </p>
+            <button onClick={displayRandomQuote}>Inspire Me!</button>
+            {quote && <p>{quote}</p>}
+        </>
+    )
 }
 
-export default tail
+export default Tail;
