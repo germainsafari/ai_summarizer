d4af4f478e41da09dc3614a8cc8634a30d6242b6
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-18 17:07:53 +0200

    touch

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import App from './App.jsx'
-import { Store } from '@reduxjs/toolkit'
-import { store } from './services/store.js'
+import App from "./App";
+import { store } from "./services/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
- 
+  </React.StrictMode>
+);
