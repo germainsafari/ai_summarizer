ec5523beb042f02443c0f1336ba361cfa252f5bd
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-18 16:11:50 +0200

    touch

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,41 @@
-import { useState, useEffect } from "react"
-import { copy, loader, linkIcon, tick } from "../assets"
-import {useLazyGetSummaryQuery} from '../services/article'
+import { useState, useEffect } from "react";
+import { copy, loader, linkIcon, tick } from "../assets";
+import {useLazyGetSummaryQuery} from '../services/article';
 
 function Demo() {
 
   const [article, setArticle] = useState({url:'', summary:'',})
-  const handleSubmit = async(e) => {alert('submitted')};
+
+  const [AllArticles, setAllArticles] = useState([]);
+
+  const[getsummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+    if(articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+
+  },[]);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const {data} = await getsummary({articleUrl: article.url})
+  };
+
+  if (data?.summary) {
+    const newArticle = {...article, summary: data.summary};
+
+    const updatedAllArticles = [newArticle, ... allArticles];  
+
+    setArticle(newArticle);
+    console.log(newArticle);
+
+    localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
         <div className="flex flex-col w-full gap-2">
@@ -31,6 +61,12 @@ function Demo() {
             </button>
             </form>   
             
+            <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+            {allArticles.map((item, index) => (
+              div
+            ))}
+            </div>
+
         </div>
     </section>
   )
