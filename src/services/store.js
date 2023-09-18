4854b5bd53c376be0639ad08bb4477a4a6559b41
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-18 17:13:02 +0200

    touch

--- src/services/store.js
+++ src/services/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { articleApi } from "./article";
 
 export const store = configureStore({
     reducer: {
-        [articleApi.reducerPath]: articleApi.reducer
+        [articleApi.reducerPath]: articleApi.reducer,
     },
-    middleware:(getdefaultMiddleware) => getdefaultMiddleware().concat(articleApi.middleware)
-});
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
